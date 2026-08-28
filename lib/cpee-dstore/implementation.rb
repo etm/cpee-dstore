cc88e074823c316115e954bde839d13e1e5ffe8f
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2026-08-28 23:13:02 +0200

    release; more tail fixes

--- lib/cpee-dstore/implementation.rb
+++ lib/cpee-dstore/implementation.rb
@@ -28,16 +28,22 @@ class File
     lines = 0
 
     begin
-      seek(idx)
-      chunk = read(buffer)
+      reached_start = idx.zero?
+      seek idx
+      chunk = read buffer
       lines += chunk.count("\n")
-      chunks.unshift chunk
-      idx -= buffer
-    end while lines < ( n + 1 ) && pos != 0
+      chunks.unshift(chunk)
+      idx = [idx - buffer, 0].max
+    end while lines < n + 1 && !reached_start
 
     tail_of_file = chunks.join('')
-    ary = tail_of_file.split("\n")
-    ary[ ary.size - n, ary.size - 1 ].join("\n")
+    if lines < n + 1
+      tail_of_file
+    else
+      ary = tail_of_file.split("\n")
+      n = ary.size if n > ary.size
+      ary[ ary.size - n, n ].join("\n")
+    end
   end
 end
 
